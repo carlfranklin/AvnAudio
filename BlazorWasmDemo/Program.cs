a8952540dcd096aaef338e99ba9767a4cf5e7a5f
Carl Franklin <carl@franklins.net>
2026-09-01 23:48:16 -0400

    THe last commit did not update the files, somehow.

---
+++ BlazorWasmDemo/Program.cs
@@ -0,0 +1,17 @@
+global using AvnAudio;
+using BlazorWasmDemo;
+using BlazorFileSaver;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// This utility uses JavaScript to let us download a file in a WASM app
+builder.Services.AddBlazorFileSaver();
+
+// Required to use AvnAudio
+builder.Services.AddScoped<AvnAudioInterop>();
+await builder.Build().RunAsync();
